4d626fad772b6ad327c6a2e358364b8933511d64
GHAZI ALANZI <105205339+GHAZI-ALANZI@users.noreply.github.com>
2024-03-03 11:37:07 +0100

    init user model

---
+++ Regesteratio-Login-API-WithAuthenAndAuthor/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Bson;
+
+namespace Regesteratio_Login_API_WithAuthenAndAuthor.Models
+{
+    // User model
+    public class User
+    {
+        public ObjectId Id { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        // Add other user properties as needed
+    }
+
+    // Registration request model
+    public class RegistrationRequest
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        // Add other registration fields as needed
+    }
+
+    // Login request model
+    public class LoginRequest
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
+}
